06e80fc973acac1845ba404fdfcb12d293824ea4
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-05-14 16:54:51 -0300

    ajjj

--- FoodieFlow.Pgm/FoodieFlow.Pgm.SharedKernel/obj/Debug/net7.0/FoodieFlow.Pgm.SharedKernel.AssemblyInfo.cs
+++ FoodieFlow.Pgm/FoodieFlow.Pgm.SharedKernel/obj/Debug/net7.0/FoodieFlow.Pgm.SharedKernel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodieFlow.Pgm.SharedKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f007a5f63a759da0647829106e20bfcdf6ad9897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb918a50e17cf08cb1249d29959095d63d81e29")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodieFlow.Pgm.SharedKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodieFlow.Pgm.SharedKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
